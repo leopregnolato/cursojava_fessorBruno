f074d049f0e2b2e4cfd74f07ff5d9cd38d1c8aac
leopregnolato <leopregnolato@outlook.com>
2021-05-07 17:01:45 -0300

    add the Class 10 include in a folder named jogadores that contains explanations about Classes, Objects, Objects attributes and access modifiers

---
+++ jogadores/Campo.java
@@ -0,0 +1,19 @@
+package jogadores;
+
+public class Campo {
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        Membros membro = new Membros(num++);
+        Membros membroa = new Membros(num++);
+        Membros membrob = new Membros(num++);
+        Membros membroc = new Membros(num++);
+        
+        /* Membros membro = new Membros(7);
+
+        membro.num = 5;
+
+        System.out.printf("%n%d%n", membro.num);*/
+    }
+}
---
+++ jogadores/Membros.java
@@ -0,0 +1,12 @@
+package jogadores;
+
+public class Membros {
+
+    private int num=0; 
+
+    public Membros(int num){
+        this.num = num;
+        System.out.printf("Jogador de número %d criado%n", num);
+    }
+    
+}
